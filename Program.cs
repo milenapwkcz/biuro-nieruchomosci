0239ee1b3cee80b584eeda9da21c5be9403e1e02
Milena <74910387+milenapwkcz@users.noreply.github.com>
2020-12-21 22:13:21 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ namespace Biuro_nieruchomosci
         {
             Osoba o1 = new Osoba("anna", "Nowak", "02-03-1990", "00298290345", "Pro", "12", "ania@xd.pl", "881938902");
             Console.WriteLine(o1);
+            Nieruchomosc n = new Nieruchomosc("m", "m", "d", 9, 3, 3, 3, 3, true, true, true, 3, Nieruchomosc.TypNieruchomosci.dom, Nieruchomosc.RodzajNieruchomosci.prawoWlasnoscioweBezKsiegi, Nieruchomosc.TypTransakcji.sprzedaz, Nieruchomosc.Standard.doOdswiezenia, Nieruchomosc.Rynek.pierwotny);
+            WszystkieNieruchomosci razem = new WszystkieNieruchomosci();
+            razem.DodajNieruchomosc(n);
+            Console.WriteLine(razem);
+            razem.UsunNieruchomosc("1/2020");
+            Console.WriteLine(razem);
             
         }
     }
