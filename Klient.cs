564088ed2d59c81365e6b5fc6b7b3d4b594431fc
Milena <74910387+milenapwkcz@users.noreply.github.com>
2020-12-21 22:28:27 +0100

    Update Klient.cs

--- Klient.cs
+++ Klient.cs
@@ -1,10 +1,16 @@
-﻿using System;
+using System;
 namespace Biuro_nieruchomosci
 {
     public class Klient:Osoba
     {
         public Klient():base()
         {
+
+        }
+
+        public Klient(string imie, string nazwisko, string dataurodzenia, string pesel, string miejscowosc, string numerdomu, string email, string nrtelefonu) : base(imie, nazwisko, dataurodzenia, pesel, miejscowosc, numerdomu, email, nrtelefonu)
+        {
+
         }
 
         public Klient(string imie, string nazwisko, string dataurodzenia, string pesel, string miejscowosc, string ulica, string numerdomu, string email, string nrtelefonu) : base(imie, nazwisko, dataurodzenia, pesel, miejscowosc, ulica, numerdomu, email, nrtelefonu)
@@ -24,3 +30,4 @@ namespace Biuro_nieruchomosci
 
     }
 }
+
